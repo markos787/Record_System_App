2a5a5d2ea748a752f287cfa9b5fb3e196633b7ad
markos787 <marekweis01@gmail.com>
2025-08-07 19:37:46 +0200

    init: imports and class

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from tkinter import messagebox
+
+
+class Login:
+    def __init__(self, root):
+        self.root=root
+        self.root.title('Record System')
+        self.root.geometry('1350x700+0+0')
